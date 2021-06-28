c106e23489d45550b9ac76e671c1a9617d1d4acc
arakaki Tokyo <49378221+arakaki-tokyo@users.noreply.github.com>
2021-06-28 14:08:51 +0900

    Add open attribute to InputRange

--- widgets.mjs
+++ widgets.mjs
@@ -1,28 +1,50 @@
 export class InputRange extends HTMLElement {
+    _open = false;
     connectedCallback() {
         this._input = document.createElement("input");
         this._output = document.createElement("output");
 
+        // initialize input element
         this._input.setAttribute("type", "range");
         [...this.attributes].forEach(({ name, value }) => this._input.setAttribute(name, value));
 
+        // initialize output element
+        this._updateOutput();
+
+        // event handler(input or change)
         const propagate = e => {
             this.dispatchEvent(new Event(e.type, { bubbles: true }));
-            this._output.innerHTML = e.target.value;
+            this._updateOutput();
         }
         this._input.oninput = propagate;
         this._input.onchange = propagate;
 
-        this._shadowRoot = this.attachShadow({ mode: 'closed' });
-        [this._input, this._output].forEach(node => this._shadowRoot.appendChild(node));
-        this._input.dispatchEvent(new Event('input'));
+        // append element in light or shadow dom-tree.
+        if ("open" in this.attributes) {
+            this._open = true;
+            this.appendChild(this._input);
+            this.appendChild(this._output);
+        } else {
+            const shadowRoot = this.attachShadow({ mode: 'closed' });
+            shadowRoot.appendChild(this._input);
+            shadowRoot.appendChild(this._output);
+        }
 
+
+    }
+    _updateOutput() {
+        this._output.innerHTML = this._input.value;
+        const ratio = (i => (i.value - i.min) / (i.max - i.min) * 100)(this._input);
+        const adjusted = 10 - 0.2 * ratio;
+        this._output.style.left = `calc(${ratio}% + ${adjusted}px)`;
     }
     get value() { return this._input.value }
     set value(val) {
         this._input.value = val;
         this._input.dispatchEvent(new Event('input'));
     }
+    get open() { return this._open }
+    set open(val) { }
 }
 
 export class OutputFor extends HTMLElement {
