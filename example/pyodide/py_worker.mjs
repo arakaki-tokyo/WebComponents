9d615a9df1d353dfdb68c6876e1ed1e6fbdf4105
arakaki Tokyo <49378221+arakaki-tokyo@users.noreply.github.com>
2021-08-02 09:31:48 +0900

    Add python cell like jupyter -- PyCell

---
+++ example/pyodide/py_worker.mjs
@@ -0,0 +1,21 @@
+const dir = import.meta.url.match(/^.*\//g)[0];
+const pyodideWorker = new Worker(`${dir}py_worker.js`);
+let jobs = Promise.resolve();
+export function load(packages) {
+    pyodideWorker.postMessage({ function: "load", args: { packages } })
+}
+
+export async function exec(code) {
+    jobs = jobs.catch(() => undefined).then(() => new Promise((resolve, reject) => {
+        pyodideWorker.onmessage = e => {
+            if (e.data.error) {
+                reject(e.data.error);
+                jobs = Promise.resolve()
+            } else {
+                resolve(e.data);
+            }
+        }
+        pyodideWorker.postMessage({ function: "exec", args: { code } })
+    }))
+    return jobs
+}
